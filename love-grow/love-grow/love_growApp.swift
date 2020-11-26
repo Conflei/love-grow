613b2888465642b5f2629f6d7960254e82396439
Felix Izarra <felix_izarra@bose.com>
2020-11-26 13:44:53 -0500

    project files

---
+++ love-grow/love-grow/love_growApp.swift
@@ -0,0 +1,17 @@
+//
+//  love_growApp.swift
+//  love-grow
+//
+//  Created by Felix Izarra on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct love_growApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
